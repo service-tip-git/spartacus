b0a5081a350139b5c76bac3c1d2f42c421f37040
Florent Letendre <florent.letendre01@sap.com>
2024-10-31 16:01:05 -0400

    fix: Prevent 3rd party script to load ssr (#19443)
    CXSPA-8080

--- integration-libs/opf/base/root/services/opf-resource-loader.service.ts
+++ integration-libs/opf/base/root/services/opf-resource-loader.service.ts
@@ -35,12 +35,6 @@ export class OpfResourceLoaderService extends ScriptLoader {
   }): void {
     const { src, callback, errorCallback } = embedOptions;
 
-    const isSSR = isPlatformServer(this.platformId);
-
-    if (isSSR) {
-      return;
-    }
-
     const link: HTMLLinkElement = this.document.createElement('link');
     link.href = src;
     link.rel = 'stylesheet';
@@ -138,7 +132,8 @@ export class OpfResourceLoaderService extends ScriptLoader {
   }
 
   executeScriptFromHtml(html: string | undefined) {
-    if (html) {
+    // SSR mode not supported for security concerns
+    if (!isPlatformServer(this.platformId) && html) {
       const element = new DOMParser().parseFromString(html, 'text/html');
       const script = element.getElementsByTagName('script');
       if (!script?.[0]?.innerText) {
@@ -162,6 +157,11 @@ export class OpfResourceLoaderService extends ScriptLoader {
     scripts: OpfDynamicScriptResource[] = [],
     styles: OpfDynamicScriptResource[] = []
   ): Promise<void> {
+    // SSR mode not supported for security concerns
+    if (isPlatformServer(this.platformId)) {
+      return Promise.resolve();
+    }
+
     const resources: OpfDynamicScriptResource[] = [
       ...scripts.map((script) => ({
         ...script,
