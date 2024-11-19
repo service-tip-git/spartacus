4ebaf9d079067dccc6c2439e84d5849bb817766b
Hakwoo Kim <hakwoo.kim@sap.com>
2024-11-18 23:30:07 -0500

    change back logLevel

--- projects/storefrontlib/karma.conf.js
+++ projects/storefrontlib/karma.conf.js
@@ -46,7 +46,7 @@ module.exports = function (config) {
     browserNoActivityTimeout: 210000,
     port: 9876,
     colors: true,
-    logLevel: config.LOG_DEBUG,
+    logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
     customLaunchers: {
