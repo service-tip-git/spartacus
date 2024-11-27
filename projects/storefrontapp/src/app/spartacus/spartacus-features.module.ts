fe75e0af962877369800d3549bd4364a8c8b393b
PioBar <72926984+Pio-Bar@users.noreply.github.com>
2024-11-27 16:36:07 +0100

    fix: (CXSPA-7937) - Styles to differentiate between selected and focused item (#19634)

--- projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
+++ projects/storefrontapp/src/app/spartacus/spartacus-features.module.ts
@@ -404,6 +404,7 @@ if (environment.cpq) {
         a11yTextSpacingAdjustments: true,
         a11yTableHeaderReadout: true,
         a11ySearchboxAssistiveMessage: true,
+        a11yDifferentiateFocusedAndSelected: true,
         cmsBottomHeaderSlotUsingFlexStyles: true,
         useSiteThemeService: false,
         enableConsecutiveCharactersPasswordRequirement: true,
