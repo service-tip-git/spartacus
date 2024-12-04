057c7ef4cbedba41e81cec08ef6bfbba1e785662
Ulrich Stellmacher <Ulrich.Stellmacher@sap.com>
2024-12-04 15:31:14 +0100

    spike: CXSPA-8508 improve isLoaded logic, to also consider domains with size =1

--- feature-libs/product-configurator/rulebased/components/attribute/types/drop-down-lazy-load/configurator-attribute-drop-down-lazy-load.component.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/types/drop-down-lazy-load/configurator-attribute-drop-down-lazy-load.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ConfiguratorCommonsService } from '../../../../core/facade/configurator-commons.service';
 import { ConfiguratorAttributeBaseComponent } from '../base/configurator-attribute-base.component';
 import { ConfiguratorAttributeCompositionContext } from '../../composition/configurator-attribute-composition.model';
+import { Configurator } from '../../../../core/model/configurator.model';
 
 @Component({
   selector: 'cx-configurator-attribute-drop-down-lazy-load',
@@ -9,8 +10,11 @@ import { ConfiguratorAttributeCompositionContext } from '../../composition/confi
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ConfiguratorAttributeDropDownLazyLoadComponent extends ConfiguratorAttributeBaseComponent {
+  values: Configurator.Value[] =
+    this.attributeComponentContext.attribute.values ?? [];
   isLoaded: boolean =
-    (this.attributeComponentContext.attribute.values ?? []).length > 1;
+    this.values.length > 1 ||
+    (this.values.length === 1 && !this.values[0].selected);
 
   constructor(
     protected configuratorCommonsService: ConfiguratorCommonsService,
