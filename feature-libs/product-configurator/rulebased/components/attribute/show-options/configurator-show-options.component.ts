b4e5fbecca9de1394ebc4197225b5177de2cfaef
Ulrich Stellmacher <Ulrich.Stellmacher@sap.com>
2025-01-17 15:30:50 +0100

    feature: CXSPA-8968 read domain on demand

--- feature-libs/product-configurator/rulebased/components/attribute/show-options/configurator-show-options.component.ts
+++ feature-libs/product-configurator/rulebased/components/attribute/show-options/configurator-show-options.component.ts
@@ -4,14 +4,26 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ConfiguratorCommonsService } from '../../../core/facade/configurator-commons.service';
+import { ConfiguratorAttributeCompositionContext } from '../composition/configurator-attribute-composition.model';
 
 @Component({
   selector: 'cx-configurator-show-options',
   templateUrl: './configurator-show-options.component.html',
 })
 export class ConfiguratorShowOptionsComponent {
+  constructor(
+    protected configuratorCommonsService: ConfiguratorCommonsService
+  ) {}
+
+  @Input() attributeComponentContext: ConfiguratorAttributeCompositionContext;
+
   showOptions() {
-    console.log('show options');
+    this.configuratorCommonsService.readAttributeDomain(
+      this.attributeComponentContext.owner,
+      this.attributeComponentContext.group,
+      this.attributeComponentContext.attribute
+    );
   }
 }
