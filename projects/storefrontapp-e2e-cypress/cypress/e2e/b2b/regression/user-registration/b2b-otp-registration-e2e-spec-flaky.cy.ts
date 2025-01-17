ff609d0b1d56adccdf4a3ad68a0ac8b4556d9947
Grace Dong <127385561+i53577@users.noreply.github.com>
2025-01-17 16:50:56 +0800

    feat: As a customer, I want to request OTP and use it to request registration of a B2B customer on Composable Storefront (#19839)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

---
+++ projects/storefrontapp-e2e-cypress/cypress/e2e/b2b/regression/user-registration/b2b-otp-registration-e2e-spec-flaky.cy.ts
@@ -0,0 +1,96 @@
+/*
+ * SPDX-FileCopyrightText: 2025 SAP Spartacus team <spartacus-team@sap.com>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { listenForTokenAuthenticationRequest } from '../../../../helpers/login';
+
+describe('Tabbing order for B2B OTP registration', () => {
+  before(() => {
+    cy.window().then((win) => win.sessionStorage.clear());
+  });
+
+  describe('B2B OTP Registration', () => {
+    context('B2B OTP Registration page', () => {
+      beforeEach(() => {
+        cy.visit('/login/register');
+      });
+      it('should allow to navigate with tab key for otp registration form and otp verification page(CXSPA-8772)', () => {
+        cy.get('cx-user-registration-form').should('exist');
+        cy.get('[formcontrolname="titleCode"]').ngSelect('Mr.');
+        cy.get('[formcontrolname="firstName"]').type('John');
+        cy.get('[formcontrolname="lastName"]').type('Doe');
+        cy.get('[formcontrolname="companyName"]').type('My Company Inc.');
+        var email =
+          'test.test' + Math.floor(Math.random() * 10001) + '@sap.com';
+        cy.get('[formcontrolname="email"]').type(email);
+
+        cy.get('button[type=submit]').click();
+        cy.get('cx-verification-token-form').should('exist');
+        listenForUserVerficationCodeEmailReceive(email);
+        const mailCCV2Url =
+          Cypress.env('MAIL_CCV2_URL') +
+          Cypress.env('MAIL_CCV2_PREFIX') +
+          '/search?query=' +
+          email +
+          '&kind=to&start=0&limit=1';
+
+        cy.request({
+          method: 'GET',
+          url: mailCCV2Url,
+        }).then((response) => {
+          const verificationCodeEmailStartText =
+            'Please use the following verification code to register in Spartacus powertools Site:</p>';
+          const lableP = '<p>';
+          const items = response.body.items;
+          const emailBody = items[0].Content.Body;
+
+          const verificationCodeEmailStartIndex =
+            emailBody.indexOf(verificationCodeEmailStartText) +
+            verificationCodeEmailStartText.length;
+          const verificationCodeStartIndex =
+            emailBody.indexOf(lableP, verificationCodeEmailStartIndex) +
+            lableP.length;
+          const verificationCode = emailBody.substring(
+            verificationCodeStartIndex,
+            verificationCodeStartIndex + 8
+          );
+
+          listenForTokenAuthenticationRequest();
+          cy.get('cx-verification-token-form').within(() => {
+            cy.get('[formcontrolname="tokenCode"]')
+              .clear()
+              .type(verificationCode);
+            cy.get('button[type=submit]').click();
+          });
+          cy.get('cx-global-message').should('exist');
+          cy.get('cx-global-message').contains(
+            'Thank you for registering! A representative will contact you shortly and confirm your access information.'
+          );
+          cy.get('cx-login').should('exist');
+        });
+      });
+    });
+  });
+});
+
+export function listenForUserVerficationCodeEmailReceive(
+  customerEmail: string
+) {
+  const mailCCV2Url =
+    Cypress.env('MAIL_CCV2_URL') +
+    Cypress.env('MAIL_CCV2_PREFIX') +
+    '/search?query=' +
+    customerEmail +
+    '&kind=to';
+
+  cy.request({
+    method: 'GET',
+    url: mailCCV2Url,
+  }).then((response) => {
+    if (response.body.total != 1) {
+      listenForUserVerficationCodeEmailReceive(customerEmail);
+    }
+  });
+}
