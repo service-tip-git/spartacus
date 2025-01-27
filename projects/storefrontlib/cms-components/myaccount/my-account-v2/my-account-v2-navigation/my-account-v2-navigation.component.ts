cceccd76d0fd67dcefcf5265ebc54dddb726d044
Kar <j.kar@sap.com>
2025-01-27 23:27:02 +0530

    fix based on myaccount

--- projects/storefrontlib/cms-components/myaccount/my-account-v2/my-account-v2-navigation/my-account-v2-navigation.component.ts
+++ projects/storefrontlib/cms-components/myaccount/my-account-v2/my-account-v2-navigation/my-account-v2-navigation.component.ts
@@ -6,9 +6,15 @@
 
 import { Component } from '@angular/core';
 import { NavigationComponent } from '../../../navigation';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'cx-my-account-v2-navigation',
   templateUrl: './my-account-v2-navigation.component.html',
 })
-export class MyAccountV2NavigationComponent extends NavigationComponent {}
+export class MyAccountV2NavigationComponent extends NavigationComponent {
+  navAriaLabel$: Observable<any> = this.name$.pipe(
+    map((name: any) => name ?? 'MyAccount View Side Navigation')
+  );
+}
