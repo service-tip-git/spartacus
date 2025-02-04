e6dcc9129d6e860e88155ffa70fc4623f87c2612
Radhep Sabapathipillai <radhep.sabapathipillai@sap.com>
2025-02-04 13:24:38 -0500

    test

--- ci-scripts/check-translation.sh
+++ ci-scripts/check-translation.sh
@@ -13,16 +13,10 @@ fi
 # Get the list of translation folders
 translation_folders=$(find "$folder_path" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
 
-# Convert string to lowercase
-to_lowercase() {
-  echo "$1" | tr '[:upper:]' '[:lower:]'
-}
-
 # Check if each folder and index file is listed in the translations file
 for folder in $translation_folders; do
-  lowercase_folder=$(to_lowercase "$folder")
-  lowercase_export_statement=$(to_lowercase "export { $folder as cpqquoteTranslations${folder^} } from './$folder/index';")
-  if ! grep -qi "$lowercase_export_statement" "$translations_file"; then
+  statement="from './$folder/index';"
+  if ! grep -q "$statement" "$translations_file"; then
     echo "Export statement for folder $folder not found in $translations_file"
     # Add your logic here to update the translations.ts file if needed
   else
