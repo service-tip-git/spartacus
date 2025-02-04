c2f5f801a6b43aa1b7950a52975e50537cf6b6d2
Radhep Sabapathipillai <radhep.sabapathipillai@sap.com>
2025-02-04 13:39:16 -0500

    test

--- ci-scripts/check-translation.sh
+++ ci-scripts/check-translation.sh
@@ -22,4 +22,22 @@ for folder in $translation_folders; do
   else
     echo "Export statement for folder $folder is already listed in $translations_file"
   fi
+
+  # Check if each index.ts file contains the correct import statements for .json files
+  index_file="$folder_path/$folder/index.ts"
+  if [ ! -f "$index_file" ]; then
+    echo "Index file not found: $index_file"
+    exit 1
+  fi
+
+  json_files=$(find "$folder_path/$folder" -maxdepth 1 -type f -name "*.json" -exec basename {} \;)
+  for json_file in $json_files; do
+    json_statement="from './$json_file';"
+    if ! grep -q "$json_statement" "$index_file"; then
+      echo "Import statement for $json_file not found in $index_file"
+      exit 1
+    else
+      echo "Import statement for $json_file is already listed in $index_file"
+    fi
+  done
 done
