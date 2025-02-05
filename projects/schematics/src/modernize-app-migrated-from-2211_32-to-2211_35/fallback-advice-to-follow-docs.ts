f5f09b6b310aec625bb8b3ef67ee9c469a5b6b76
Krzysztof Platis <platonn.git@gmail.com>
2025-02-06 00:51:44 +0100

    wording of FALLBACK_ADVICE_TO_FOLLOW_DOCS + dont export it

--- projects/schematics/src/modernize-app-migrated-from-2211_32-to-2211_35/fallback-advice-to-follow-docs.ts
+++ projects/schematics/src/modernize-app-migrated-from-2211_32-to-2211_35/fallback-advice-to-follow-docs.ts
@@ -6,7 +6,7 @@ import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 export const DOCS_URL_FOR_MODERNIZING_APPS_MIGRATED_FROM_2211_32_TO_2211_35 =
   'https://help.sap.com/docs/SAP_COMMERCE_COMPOSABLE_STOREFRONT/10a8bc7f635b4e3db6f6bb7880e58a7d/525dd86b812544249e78d73f67f3e6ed.html';
 
-export const FALLBACK_ADVICE_TO_FOLLOW_DOCS = `Could not update this file automatically. To complete the migration, please follow the manual steps: ${DOCS_URL_FOR_MODERNIZING_APPS_MIGRATED_FROM_2211_32_TO_2211_35}`;
+const FALLBACK_ADVICE_TO_FOLLOW_DOCS = `Could not complete this step automatically. To complete the migration, please follow the manual steps: ${DOCS_URL_FOR_MODERNIZING_APPS_MIGRATED_FROM_2211_32_TO_2211_35}`;
 
 /**
  * Prints an error message and a link to the manual migration steps.
