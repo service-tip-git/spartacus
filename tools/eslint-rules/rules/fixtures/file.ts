83e49d39acbc041519e51d37d54d7d1ec5ca6644
Pawel Fras <fras.pawel@yahoo.com>
2025-02-05 12:16:25 +0100

    test: adjust unit tests

---
+++ tools/eslint-rules/rules/fixtures/file.ts
@@ -0,0 +1,2 @@
+//required for type-aware testing
+//for more, see https://typescript-eslint.io/packages/rule-tester#type-aware-testing
--- tools/eslint-rules/rules/ngrx-fail-action-must-initialize-error.spec.ts
+++ tools/eslint-rules/rules/ngrx-fail-action-must-initialize-error.spec.ts
@@ -4,76 +4,236 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import {
-  convertAnnotatedSourceToFailureCase,
-  RuleTester,
-} from '@angular-eslint/test-utils';
+import { RuleTester } from '@angular-eslint/test-utils';
+import { join } from 'path';
 import { rule, RULE_NAME } from './ngrx-fail-action-must-initialize-error';
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+  languageOptions: {
+    parserOptions: {
+      project: '../../tsconfig.spec.json',
+      tsconfigRootDir: join(__dirname, '/fixtures'),
+    },
+  },
+});
 
 ruleTester.run(RULE_NAME, rule, {
   valid: [
-    `
-    export class SearchProductsFail implements ErrorAction {
-        readonly type = SEARCH_PRODUCTS_FAIL;
-        error = 'some error'; // Property initializer
-    }
-    `,
-    `
-    export class SearchProductsFail implements ErrorAction {
-        readonly type = SEARCH_PRODUCTS_FAIL;
-        constructor(public error: any) {} // Constructor parameter
-    }
-    `,
-    `
-    export class SearchProductsFail implements ErrorAction {
-        readonly type = SEARCH_PRODUCTS_FAIL;
-        error: any;
-        constructor(public payload: any) {
-            this.error = payload; // Constructor assignment
+    {
+      name: 'Property initializer',
+      code: `
+        export interface ErrorAction {
+          error: Object;
         }
-    }
-    `,
-    `
-    export class SearchProductsFail implements ErrorAction {
-        readonly type = SEARCH_PRODUCTS_FAIL;
-        error: any;
-        constructor() {
-            this.error = 'error'; // Constructor body assignment
-        }
-    }
-    `,
-    `
-    export class SearchProductsFail {
-        readonly type = SEARCH_PRODUCTS_FAIL;
-      constructor(public payload: any) {}
-    }
-    `,
-  ],
-  invalid: [
-    convertAnnotatedSourceToFailureCase({
-      description: 'Error property declared but not initialized in constructor',
-      annotatedSource: `
+
         export class SearchProductsFail implements ErrorAction {
-                     ~~~~~~~~~~~~~~~~~~
-          readonly type = SEARCH_PRODUCTS_FAIL;
-          error: any;
+            readonly type = SEARCH_PRODUCTS_FAIL;
+            error = 'some error'; // Property initializer
+        }
+      `,
+      filename: 'file.ts',
+    },
+    {
+      name: 'Constructor parameter',
+      code: `
+        export interface ErrorAction {
+          error: Object;
+        }
+
+        export class SearchProductsFail implements ErrorAction {
+            readonly type = SEARCH_PRODUCTS_FAIL;
+            constructor(public error: any) {} // Constructor parameter
+        }
+      `,
+      filename: 'file.ts',
+    },
+    {
+      name: 'Constructor assignment',
+      code: `
+        export interface ErrorAction {
+          error: Object;
+        }
+
+        export class SearchProductsFail implements ErrorAction {
+            readonly type = SEARCH_PRODUCTS_FAIL;
+            error: Object;
+            constructor(public payload: any) {
+                this.error = payload; // Constructor assignment
+            }
+        }
+      `,
+      filename: 'file.ts',
+    },
+    {
+      name: 'Constructor body assignment',
+      code: `
+        export interface ErrorAction {
+          error: Object;
+        }
+
+        export class SearchProductsFail implements ErrorAction {
+            readonly type = SEARCH_PRODUCTS_FAIL;
+            error: any;
+            constructor() {
+                this.error = 'error'; // Constructor body assignment
+            }
+        }
+      `,
+      filename: 'file.ts',
+    },
+    {
+      name: 'Class not implementing ErrorAction',
+      code: `
+        export interface ErrorAction {
+          error: Object;
+        }
+
+        export class SearchProductsFail {
+            readonly type = SEARCH_PRODUCTS_FAIL;
           constructor(public payload: any) {}
         }
       `,
-      messageId: 'missingErrorInitialization',
-    }),
-    convertAnnotatedSourceToFailureCase({
-      description: 'Error property declared but not initialized at all',
-      annotatedSource: `
-        export class SearchProductsFail implements ErrorAction {
-                     ~~~~~~~~~~~~~~~~~~
-          readonly type = SEARCH_PRODUCTS_FAIL;
-          error: any;
+      filename: 'file.ts',
+    },
+    {
+      name: 'Class with Fail but not implementing ErrorAction',
+      code: `
+        export class LoadProductsFail {
+            readonly type = LOAD_PRODUCTS_FAIL;
+            constructor(public payload: Error) {}
         }
       `,
-      messageId: 'missingErrorInitialization',
-    }),
+      filename: 'file.ts',
+    },
+    {
+      name: 'Multiple interfaces implementation',
+      code: `
+        export interface ErrorAction { 
+          error: Object; 
+        }
+        
+        export interface PayloadAction { 
+          payload: any; 
+        }
+        
+        export class ComplexFail implements ErrorAction, PayloadAction {
+          readonly type = COMPLEX_FAIL;
+          constructor(public payload: any) {
+            this.error = payload;
+          }
+        }
+      `,
+      filename: 'file.ts',
+    },
+    {
+      name: 'Inheritance case',
+      code: `
+        export interface ErrorAction { error: Object; }
+        export class BaseFailAction implements ErrorAction {
+          error = new Error();
+        }
+        export class DerivedFail extends BaseFailAction {
+          readonly type = DERIVED_FAIL;
+        }
+      `,
+      filename: 'file.ts',
+    },
+  ],
+  invalid: [
+    {
+      name: 'Missing error initialization in class',
+      code: `
+        export interface ErrorAction {
+          error: Object;
+        }
+
+        export class SearchProductsFail implements ErrorAction {
+          readonly type = SEARCH_PRODUCTS_FAIL;
+          error: Object;
+          constructor(public payload: any) {}
+        }
+      `,
+      errors: [
+        { messageId: 'missingErrorInitialization', line: 6, column: 22 },
+      ],
+      filename: 'file.ts',
+    },
+    {
+      name: 'Missing error initialization in class',
+      code: `
+        export interface ErrorAction {
+          error: Object;
+        }
+
+        export class SearchProductsFail implements ErrorAction {
+          readonly type = SEARCH_PRODUCTS_FAIL;
+          error: Object;
+        }
+      `,
+      errors: [
+        { messageId: 'missingErrorInitialization', line: 6, column: 22 },
+      ],
+      filename: 'file.ts',
+    },
+    {
+      name: 'Missing error initialization in inherited class',
+      code: `
+        export interface ErrorAction { 
+          error: Object; 
+        }
+        
+        export class BaseFailAction implements ErrorAction {
+          error: Error;
+        }
+       
+        export class DerivedFail extends BaseFailAction {
+          readonly type = DERIVED_FAIL;
+        }
+      `,
+      errors: [
+        { messageId: 'missingErrorInitialization', line: 6, column: 22 }, // BaseFailAction
+        { messageId: 'missingErrorInitialization', line: 10, column: 22 }, // DerivedFail
+      ],
+      filename: 'file.ts',
+    },
+    {
+      name: 'Multiple interfaces with uninitialized error',
+      code: `
+        export interface ErrorAction { 
+          error: Object;
+        }
+        
+        export interface PayloadAction { 
+          payload: any; 
+        }
+        
+        export class ComplexFail implements ErrorAction, PayloadAction {
+          readonly type = COMPLEX_FAIL;
+          payload = {};
+          error: Object;
+        }
+      `,
+      errors: [
+        { messageId: 'missingErrorInitialization', line: 10, column: 22 }, // ComplexFail
+      ],
+      filename: 'file.ts',
+    },
+    {
+      name: 'Typed error property without initialization',
+      code: `
+        export interface ErrorAction { 
+          error: Object;
+        }
+        
+        export class TypedFail implements ErrorAction {
+          readonly type = TYPED_FAIL;
+          error: Object;
+        }
+      `,
+      errors: [
+        { messageId: 'missingErrorInitialization', line: 6, column: 22 },
+      ],
+      filename: 'file.ts',
+    },
   ],
 });
