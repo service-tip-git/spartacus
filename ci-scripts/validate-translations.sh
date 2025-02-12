21143b2bb265f754fe36c5c8bd6e58b13ab1cde8
Radhep Sabapathipillai <radhep.sabapathipillai@sap.com>
2025-02-12 14:36:49 -0500

    adjust script to run all translation folders on default

--- ci-scripts/validate-translations.sh
+++ ci-scripts/validate-translations.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 # scripts/handle-translation-changes.sh
 
-folder_path=$1
-translations_file="$folder_path/translations.ts"
-
 # Function to check if an export statement exists in a file
 check_export_statements() {
   local folder=$1
@@ -35,14 +32,35 @@ check_import_statements() {
   done
 }
 
-# Ensure the translations file exists
-[ -f "$translations_file" ] || { echo "Translations file not found: $translations_file"; exit 1; }
+# Function to process a translation folder
+process_translation_folder() {
+  local folder_path=$1
+  local translations_file="$folder_path/translations.ts"
 
-# Get the list of translation folders
-translation_folders=$(find "$folder_path" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
+  # Ensure the translations file exists
+  [ -f "$translations_file" ] || { echo "Translations file not found: $translations_file"; exit 1; }
 
-# Check if each folder and index file is listed in the translations file
-for folder in $translation_folders; do
-  check_export_statements "$folder" "$translations_file"
-  check_import_statements "$folder"
-done
+  # Get the list of translation folders
+  translation_folders=$(find "$folder_path" -mindepth 1 -maxdepth 1 -type d -exec basename {} \;)
+
+  # Check if each folder and index file is listed in the translations file
+  for folder in $translation_folders; do
+    check_export_statements "$folder" "$translations_file"
+    check_import_statements "$folder"
+  done
+}
+
+# Check if folder_path is provided as an argument
+if [ -z "$1" ]; then
+  # Find all translation folders in the repository
+  translation_paths=$(find . -type d -path '*/assets/translations')
+
+  for path in $translation_paths; do
+    process_translation_folder "$path"
+  done
+else
+  folder_path=$1
+  process_translation_folder "$folder_path"
+fi
+
+echo "Translations all successfully validated"
