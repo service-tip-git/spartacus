5bfb106bbbb9e5019f331cb40be134baa5c8c1a4
Pawel Fras <fras.pawel@yahoo.com>
2025-02-21 11:40:50 +0100

    feat: CXSPA-9318 [PoC] Migrate storefrontapp to 'application' builder

---
+++ esbuild/plugins.ts
@@ -0,0 +1,26 @@
+import { Plugin } from 'esbuild';
+
+const keyPrefix = 'CX_';
+const env: Record<string, unknown> = {};
+
+const keys = Object.keys(process.env).filter((key) =>
+  key.startsWith(keyPrefix)
+);
+
+keys.forEach((key) => {
+  const value = process.env[key];
+  env[key] = value === 'true' ? true : value === 'false' ? false : value;
+});
+
+const resolveEnvPlugin: Plugin = {
+  name: 'resolve-env-plugin',
+  setup(build) {
+    const config = build.initialOptions;
+    config.define = {
+      ...config.define,
+      'buildProcess.env': JSON.stringify(env),
+    };
+  },
+};
+
+export default [resolveEnvPlugin];
